953c5cbc37b2727ab83c36f693b6fb1a354d50f1
Marton Szitas <szitas.marton.tamas@gmail.com>
2023-09-27 14:47:14 +0200

    Retrieving blog posts data from file system works

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,9 +1,33 @@
 import PostContent from '../../components/posts/post-detail/post-content';
+import { getPostData, getPostsFiles } from '../../lib/posts-util';
 
-function PostDetailPage() {
+function PostDetailPage(props) {
   return (
-    <PostContent />
+    <PostContent post={props.post}/>
   );
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600
+  };
+}
+
+export function getStaticPaths() {       // "getStaticPath" >> for the purpose to let NextJS know which concrete "slug" values it should pregenerate!
+  const postFilenames = getPostsFiles();
+  const slugs = postFilenames.map((fileName) => ( fileName.replace(/\.md$/, '') ));
+
+  return {
+    paths: slugs.map((slugData) => ({ params: { slug: slugData } })),     // we are pregenerating all paths in advance with all "slugs"
+    fallback: false
+  };
 }
 
 export default PostDetailPage;
