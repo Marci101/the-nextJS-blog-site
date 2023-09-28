460a18bed3b8a9db6b22c91a41b7a6ebe357ca68
Marton Szitas <szitas.marton.tamas@gmail.com>
2023-09-28 13:05:38 +0200

    Contact page wired to MongoDB

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,15 @@
-function handler(req, res) {
+import { connectDatabase, insertDocument } from '../../lib/db-util';
+
+async function handler(req, res) {
+
+  let client;
+  try {
+    client = await connectDatabase();
+  } catch (error) {
+    res.status(500).json({ message: 'Connecting to the database failed!', error: error.message });
+    return;
+  }
+
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -10,21 +21,27 @@ function handler(req, res) {
       !message ||
       message.trim() === ''
     ) {
-      res.status(422).json({
-        message: 'Invalid input!',
-      });
+      res.status(422).json({ message: 'Invalid input!' });
+      client.close();
       return;
     }
 
-    // Store it in a database
     const newMessage = {
       email,
       name,
       message
     };
 
-    res.status(201).json({ message: 'Successfully stored message!', data: newMessage });
+    try {
+      const result = await insertDocument(client, 'messages', newMessage);
+      newMessage.id = result.insertedId;
+      res.status(201).json({ message: 'Successfully stored message!', data: newMessage });
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting data failed!', error: error.message });
+      client.close();
+    }
   }
+  client.close();
 }
 
 export default handler;
