25f2a4edc6863340e66238b565c073e51aa6d66a
Marton Szitas <szitas.marton.tamas@gmail.com>
2023-09-28 20:52:49 +0200

    Meta data in Head added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
-import Hero from "../components/home-page/hero";
-import FeaturedPosts from "../components/home-page/featured-posts";
+import Head from 'next/head';
+import Hero from '../components/home-page/hero';
+import FeaturedPosts from '../components/home-page/featured-posts';
 import { getFeaturedPosts } from '../lib/posts-util';
 
 function HomePage(props) {
   return (
     <>
+      <Head>
+        <meta
+          name='description'
+          content='I blog about NextJS and React'
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,10 +1,19 @@
+import Head from 'next/head';
 import AllPosts from '../../components/posts/all-posts';
 
 import { getAllPosts } from '../../lib/posts-util';
 
 function AllPostsPage(props) {
   return (
-    <AllPosts posts={props.posts} />
+    <>
+      <Head>
+        <meta
+          name='description'
+          content='A list of all web development related blog posts!'
+        />
+      </Head>
+      <AllPosts posts={props.posts} />
+    </>
   );
 }
 
